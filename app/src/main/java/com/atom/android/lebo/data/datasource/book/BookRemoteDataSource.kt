9c5e24389d56b77c89e1c60e520f5061eed1c657
hungvv-1301 <vy.van.hung@sun-asterisk.com>
2022-11-23 12:53:30 +0700

    task/create_home_screen

---
+++ app/src/main/java/com/atom/android/lebo/data/datasource/book/BookRemoteDataSource.kt
@@ -0,0 +1,28 @@
+package com.atom.android.lebo.data.datasource.book
+
+import com.atom.android.lebo.base.BaseResponse
+import com.atom.android.lebo.data.api.service.UnAuthApiService
+import com.atom.android.lebo.model.Book
+import com.atom.android.lebo.model.Genre
+import com.atom.android.lebo.model.Slider
+import io.reactivex.rxjava3.core.Single
+
+class BookRemoteDataSource(private val bookService: UnAuthApiService.BookService) :
+    IBookDataSource.Remote {
+
+    override fun getGenre(): Single<BaseResponse<List<Genre>?>> {
+        return bookService.getGenre()
+    }
+
+    override fun getBook(page: Int): Single<BaseResponse<List<Book>?>> {
+        return bookService.getBook(page)
+    }
+
+    override fun getSlider(): Single<BaseResponse<List<Slider>?>> {
+        return bookService.getSlider()
+    }
+
+    override fun getBookByID(id: Int): Single<BaseResponse<Book?>> {
+        return bookService.getBookByID(id)
+    }
+}
